b600a21ace9a0f7c8c20b6d8c6689f9ae7d798d1
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2024-07-19 11:49:51 -0600

    controler y model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,8 +3,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+// VISTAS
 $routes->get('/', 'Home::index');
-// regiones
 $routes->get('ver_planes', 'PlanesController::index');
 $routes->get('ver_lineas', 'LineasTelefonicasController::index');
 $routes->get('ver_clientes', 'ClientesController::index');
@@ -18,6 +18,6 @@ $routes->post('agregar_cliente', 'ClientesController::agregarCliente');
 // Lineas de telefonia
 $routes->get('nueva_linea', 'LineasTelefonicasController::nuevaLinea');
 $routes->post('agregar_linea', 'LineasTelefonicasController::agregarLinea');
-/* RUTA PARA ELIMINAR */
+/* --------- RUTA PARA ELIMINAR -------*/
 $routes->get('eliminar_cliente/(:num)','ClientesController::eliminarCliente/$1');
 
