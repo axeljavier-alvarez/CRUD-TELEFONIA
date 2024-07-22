0a4b13a2bc84e4f4c18a53d1985587aa50357e33
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2024-07-22 14:18:08 -0600

    nuevos archivos

--- app/Views/index.php
+++ app/Views/index.php
@@ -5,18 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boostrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 </head>
 <body>
- 
 <h1>Menú principal</h1>
-
 <a href="ver_planes" class="btn btn-primary">Planes</a>
 <a href="ver_lineas" class="btn btn-danger">Lineas telefonicas</a>
 <a href="ver_clientes" class="btn btn-success">Clientes</a>
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </body>
 </html>
